c3e42f0e9d3dcdcf52443a6f73d7057eeff2b081
ultr4 <cl.rch09@icloud.com>
2024-03-23 00:09:04 -0300

    refactor: Atualiza componente RenderMenuItem para usar o componente Link do react-router-dom

--- src/components/render-menu-item.tsx
+++ src/components/render-menu-item.tsx
@@ -1,11 +1,18 @@
-const RenderMenuItem = (IconComponent: React.ElementType, label: string) => {
+import { Link } from 'react-router-dom'
+
+const MenuItem = (IconComponent: React.ElementType, label: string) => {
   return (
     <div className="flex h-20 w-full items-center gap-4 bg-black p-4">
       <div className="rounded-full border border-lime-500 bg-lime-500 p-3">
         <IconComponent className="text-white" size={24} />
       </div>
-      <span className="text-lg font-medium text-white">{label}</span>
+      <Link
+        to={`/${label}`}
+        className="cursor-pointer text-lg font-medium text-white transition duration-200 hover:text-lime-600"
+      >
+        {label}
+      </Link>
     </div>
   )
 }
-export default RenderMenuItem
+export default MenuItem
