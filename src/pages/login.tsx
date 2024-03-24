b48f4cc5bf5fe3ce1781141ead823a3edc0393df
ultr4 <cl.rch09@icloud.com>
2024-03-24 20:19:52 -0300

    chore: Alterar formulário de login para formulário de registro

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,4 @@
-import LoginForm from '../components/login-form'
+import RegisterForm from '../components/register-form'
 
 const Login: React.FC = () => {
   return (
@@ -14,7 +14,8 @@ const Login: React.FC = () => {
             className="hidden h-screen w-full object-cover object-center lg:block"
           />
         </div>
-        <LoginForm />
+        {/* <LoginForm /> */}
+        <RegisterForm />
       </div>
     </>
   )
