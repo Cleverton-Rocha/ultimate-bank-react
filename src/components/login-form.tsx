5bba12dd02b87bab80fe4923970a5eb30ad4348a
ultr4 <cl.rch09@icloud.com>
2024-03-24 17:56:18 -0300

    feat: Atualiza validação e formatação do CPF no formulário de login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,12 +1,14 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight } from 'lucide-react'
-import { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { z } from 'zod'
 
 const loginSchema = z.object({
-  CPF: z.string().length(11, { message: 'CPF must have 11 characters.' }),
+  CPF: z
+    .string()
+    .length(14, { message: 'Write a valid CPF.' })
+    .transform((CPF) => CPF.replace(/\D/g, '')),
   password: z
     .string()
     .min(8, { message: 'Password must have at least 8 characters.' }),
@@ -15,26 +17,35 @@ const loginSchema = z.object({
 type LoginFormValues = z.infer<typeof loginSchema>
 
 const LoginForm: React.FC = () => {
-  const [loading, setLoading] = useState(false)
-
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<LoginFormValues>({
     resolver: zodResolver(loginSchema),
-    defaultValues: {
-      CPF: '',
-      password: '',
-    },
   })
 
-  const navigate = useNavigate()
+  const formatCPF = (CPF: string) => {
+    let value = CPF.replace(/\D/g, '')
+    value = value.slice(0, 11)
 
-  const onSubmit = (values: LoginFormValues) => {
-    console.log(values)
-    setLoading(true)
-    navigate('/home')
+    if (value.length > 3 && value.length <= 6) {
+      value = `${value.slice(0, 3)}.${value.slice(3)}`
+    } else if (value.length > 6 && value.length <= 9) {
+      value = `${value.slice(0, 3)}.${value.slice(3, 6)}.${value.slice(6)}`
+    } else if (value.length > 9) {
+      value = `${value.slice(0, 3)}.${value.slice(3, 6)}.${value.slice(6, 9)}-${value.slice(9)}`
+    }
+    return value
+  }
+
+  const handleCPFInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const CPF = e.target
+    CPF.value = formatCPF(CPF.value)
+  }
+
+  const onSubmit = (data: LoginFormValues) => {
+    console.log(data)
   }
 
   return (
@@ -46,7 +57,7 @@ const LoginForm: React.FC = () => {
           </div>
 
           <div className="flex flex-col gap-8">
-            <form onSubmit={handleSubmit((d) => onSubmit(d))}>
+            <form onSubmit={handleSubmit((data) => onSubmit(data))}>
               <div>
                 <label
                   htmlFor="CPF"
@@ -55,8 +66,11 @@ const LoginForm: React.FC = () => {
                   CPF
                 </label>
                 <input
+                  type="text"
                   {...register('CPF')}
                   className="mb-2 mt-3 w-full border-b border-white border-opacity-20 bg-black py-2 text-lg font-semibold outline-none"
+                  maxLength={14}
+                  onInput={handleCPFInput}
                 />
                 <div className="text-red-500">
                   {errors.CPF?.message && <>{errors.CPF?.message}</>}
@@ -78,7 +92,6 @@ const LoginForm: React.FC = () => {
                 <div className="text-red-500">
                   {errors.password?.message && <>{errors.password?.message}</>}
                 </div>
-                <br />
                 <Link
                   to={'/forgot-password'}
                   className="text-lime-600 transition duration-200 hover:text-lime-700"
@@ -90,7 +103,6 @@ const LoginForm: React.FC = () => {
               <div className="mt-14 flex justify-center">
                 <button
                   type="submit"
-                  disabled={loading}
                   className="w-full rounded-full bg-lime-600 p-3 text-xl font-semibold text-white transition duration-200 hover:bg-lime-700"
                 >
                   <div className="flex items-center justify-center gap-3">
