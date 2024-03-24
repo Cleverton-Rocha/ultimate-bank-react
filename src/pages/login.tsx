e05dd8fe675fd9f54fb94e93cca5cc49407516b2
ultr4 <cl.rch09@icloud.com>
2024-03-23 22:50:22 -0300

    chore: Atualiza o componente Login para usar o tipo React.FC

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import LoginForm from '../components/login-form'
 
-const Login = () => {
+const Login: React.FC = () => {
   return (
     <>
       <div className="grid grid-cols-1 bg-black md:grid-cols-1 lg:grid-cols-2">
