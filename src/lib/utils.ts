54d2555c1e7c9414c559355ade7f23fec46cdc1c
ultr4 <cl.rch09@icloud.com>
2024-03-24 20:19:15 -0300

    feat: Atualizações no arquivo utils.ts: remoção de importações não utilizadas e adição de funções para formatar CPF e manipular entrada de nome

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+// import { type ClassValue, clsx } from 'clsx'
+// import { twMerge } from 'tailwind-merge'
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+// export function cn(...inputs: ClassValue[]) {
+//   return twMerge(clsx(inputs))
+// }
+
+export const formatCPF = (CPF: string) => {
+  let value = CPF.replace(/\D/g, '')
+  value = value.slice(0, 11)
+
+  if (value.length > 3 && value.length <= 6) {
+    value = `${value.slice(0, 3)}.${value.slice(3)}`
+  } else if (value.length > 6 && value.length <= 9) {
+    value = `${value.slice(0, 3)}.${value.slice(3, 6)}.${value.slice(6)}`
+  } else if (value.length > 9) {
+    value = `${value.slice(0, 3)}.${value.slice(3, 6)}.${value.slice(6, 9)}-${value.slice(9)}`
+  }
+  return value
+}
+
+export const handleCPFInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const CPF = e.target
+  CPF.value = formatCPF(CPF.value)
+}
+
+export const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const name = e.target
+  name.value = name.value.replace(/[0-9]/g, '')
 }
