0531fae3ee0ac705436f1e5a5de74619ede00f97
ultr4 <cl.rch09@icloud.com>
2024-03-25 12:34:14 -0300

    feat: Adiciona formatação de valor e tratamento de entrada de CPF

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -24,6 +24,21 @@ export const handleCPFInput = (e: React.ChangeEvent<HTMLInputElement>) => {
   CPF.value = formatCPF(CPF.value)
 }
 
+export const formatAmount = (amount: string) => {
+  let value = amount.replace(/\D/g, '')
+  value = `$${value.slice(0, 6)}`
+
+  if (value.length > 3) {
+    value = `${value.slice(0, -3)}.${value.slice(-3)}`
+  }
+  return value
+}
+
+export const handleAmountInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const amount = e.target
+  amount.value = formatAmount(amount.value)
+}
+
 export const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
   const name = e.target
   name.value = name.value.replace(/[0-9]/g, '')
