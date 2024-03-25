1afbe7df1d4922cc71dc31acf2b01892497b69a6
ultr4 <cl.rch09@icloud.com>
2024-03-25 00:10:03 -0300

    refactor: Atualiza cor de fundo do formulário de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,7 +4,7 @@ import LoginForm from '../components/login-form'
 const Login: React.FC = () => {
   return (
     <>
-      <div className="grid grid-cols-1 bg-black md:grid-cols-1 lg:grid-cols-2">
+      <div className="grid grid-cols-1 bg-zinc-950 md:grid-cols-1 lg:grid-cols-2">
         <Header />
         <LoginForm />
       </div>
