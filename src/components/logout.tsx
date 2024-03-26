539d7aa4709ece6046b23862b2a43a4981185ad0
ultr4 <cl.rch09@icloud.com>
2024-03-26 11:56:44 -0300

    feat: Adiciona componente de logout

---
+++ src/components/logout.tsx
@@ -0,0 +1,25 @@
+import { ArrowLeft } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
+
+const Logout = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    navigate('/')
+    // Remove token from cookies
+  }
+
+  return (
+    <>
+      <div
+        onClick={() => handleLogout()}
+        className="flex h-24 w-32 cursor-pointer items-center gap-1 p-5 text-white transition duration-200 hover:text-lime-500"
+      >
+        <ArrowLeft className="mt-0.5" size={23} />
+        <span className="text-xl">Logout</span>
+      </div>
+    </>
+  )
+}
+
+export default Logout
