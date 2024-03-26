2f15b0a228b32eb477009e1e05efc1c8cba9f9a4
ultr4 <cl.rch09@icloud.com>
2024-03-26 00:25:54 -0300

    feat: Atualizações no componente MenuItem

--- src/components/menu-item.tsx
+++ src/components/menu-item.tsx
@@ -1,23 +1,35 @@
 import { Link } from 'react-router-dom'
 
+import { cn } from '../lib/utils'
+
 type MenuItemProps = {
   Icon: React.ElementType
+  href: string
   label: string
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ Icon, label }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ Icon, href, label }) => {
+  const { pathname } = window.location
+
+  const isActive = href === pathname
+
   return (
-    <div className="flex h-20 w-full items-center gap-4 bg-zinc-950 p-4">
-      <div className="rounded border border-lime-500 bg-lime-500 p-3">
-        <Icon className="text-white" size={24} />
+    <nav className="flex h-20 w-full items-center gap-4 bg-zinc-950 p-4">
+      <div className="flex items-center gap-2">
+        <div className="rounded border border-lime-600 bg-lime-600 p-2">
+          <Icon className="text-white" size={25} />
+        </div>
+        <Link
+          to={href}
+          className={cn(
+            'cursor-pointer text-lg font-medium text-white transition duration-200 hover:text-lime-600',
+            isActive ? 'text-lime-500' : '',
+          )}
+        >
+          {label}
+        </Link>
       </div>
-      <Link
-        to={`/${label.toLocaleLowerCase()}`}
-        className="cursor-pointer text-lg font-medium text-white transition duration-200 hover:text-lime-600"
-      >
-        {label}
-      </Link>
-    </div>
+    </nav>
   )
 }
 export default MenuItem
