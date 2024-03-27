3d6d578a7baee494473b2e50cd4f98cf131c2298
ultr4 <cl.rch09@icloud.com>
2024-03-27 00:04:21 -0300

    feat: Adiciona página de saque

---
+++ src/pages/withdraw.tsx
@@ -0,0 +1,26 @@
+import Balance from '../components/balance'
+import SideMenu from '../components/side-menu'
+import WithdrawForm from '../components/withdraw-form'
+
+const Withdraw = () => {
+  return (
+    <>
+      <div className="flex h-screen w-full">
+        <SideMenu />
+
+        <div className="h-screen w-full bg-zinc-950">
+          <div className="flex h-20 items-center bg-zinc-900 text-white">
+            <span className="px-40 text-xl font-medium">Start</span>
+          </div>
+
+          <div className="flex flex-col">
+            <Balance />
+            <WithdrawForm />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Withdraw
