9a6818c99feb25cc08de7bb6ec7b3c160d9668f0
ultr4 <cl.rch09@icloud.com>
2024-03-30 18:53:11 -0300

    feat: Adiciona autenticação necessária para a página de transferência

--- src/pages/transfer.tsx
+++ src/pages/transfer.tsx
@@ -1,24 +1,27 @@
 import Balance from '../components/balance'
+import { RequireAuth } from '../components/requireAuth'
 import SideMenu from '../components/side-menu'
 import TransferForm from '../components/transfer-form'
 
 const Transfer = () => {
   return (
     <>
-      <div className="flex h-screen w-full">
-        <SideMenu />
+      <RequireAuth>
+        <div className="flex h-screen w-full">
+          <SideMenu />
 
-        <div className="h-screen w-full bg-zinc-950">
-          <div className="flex h-20 items-center bg-zinc-900 text-white">
-            <span className="px-40 text-xl font-medium">Start</span>
-          </div>
+          <div className="h-screen w-full bg-zinc-950">
+            <div className="flex h-20 items-center bg-zinc-900 text-white">
+              <span className="px-40 text-xl font-medium">Start</span>
+            </div>
 
-          <div className="flex flex-col">
-            <Balance />
-            <TransferForm />
+            <div className="flex flex-col">
+              <Balance />
+              <TransferForm />
+            </div>
           </div>
         </div>
-      </div>
+      </RequireAuth>
     </>
   )
 }
