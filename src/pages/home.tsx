5fb39d696d374d690a3354abe0a34e7555d09d6e
ultr4 <cl.rch09@icloud.com>
2024-03-30 18:52:41 -0300

    feat: Adiciona autenticação na página inicial

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,26 @@
 import Balance from '../components/balance'
 import Card from '../components/card'
+import { RequireAuth } from '../components/requireAuth'
 import SideMenu from '../components/side-menu'
 
 const Home: React.FC = () => {
   return (
-    <div className="flex h-screen w-full">
-      <SideMenu />
+    <RequireAuth>
+      <div className="flex h-screen w-full">
+        <SideMenu />
 
-      <div className="h-screen w-full bg-zinc-950">
-        <div className="flex h-20 items-center bg-zinc-900 text-white">
-          <span className="rounded-e px-40 text-xl font-medium">Start</span>
-        </div>
+        <div className="h-screen w-full bg-zinc-950">
+          <div className="flex h-20 items-center bg-zinc-900 text-white">
+            <span className="rounded-e px-40 text-xl font-medium">Start</span>
+          </div>
 
-        <div className="flex flex-col">
-          <Balance />
-          <Card />
+          <div className="flex flex-col">
+            <Balance />
+            <Card />
+          </div>
         </div>
       </div>
-    </div>
+    </RequireAuth>
   )
 }
 
