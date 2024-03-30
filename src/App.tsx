f4fb11c5dbb5c2d5428aef47910d784cf05f3ec8
ultr4 <cl.rch09@icloud.com>
2024-03-30 18:50:34 -0300

    feat: Adiciona provedor de toasts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from 'react-router-dom'
 
+import { ToasterProvider } from './providers/toast-provider'
+
 function App() {
   return (
     <>
       <Outlet />
+      <ToasterProvider />
     </>
   )
 }
