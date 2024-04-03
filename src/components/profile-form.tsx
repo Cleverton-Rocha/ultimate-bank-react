916060dbbcac817cb90869487515aa15d5a14a9f
ultr4 <cl.rch09@icloud.com>
2024-04-02 22:01:18 -0300

    feat: Atualizações no formulário de perfil

--- src/components/profile-form.tsx
+++ src/components/profile-form.tsx
@@ -2,26 +2,33 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import Cookies from 'js-cookie'
+import { useState } from 'react'
 
 import { handleNameInput } from '../lib/utils'
-import { useUser } from '../queries/user'
+import { useUpdateUser, useUser } from '../queries/user'
+
+import PasswordToggle from './password-toggle'
 
 const profileSchema = z.object({
-  name: z
+  firstName: z
     .string()
-    .min(1, { message: 'Amount must be greater than 0.' })
-    .max(8, { message: 'Amount must have at most 6 characters.' })
-    .transform((amount) => amount.replace(/\D/g, '')),
-  email: z.string().email({ message: 'Invalid email address.' }),
+    .min(1, 'Name must have at least 1 characters.')
+    .transform((firstName) => firstName.replace(/[0-9]/g, '')),
+  lastName: z
+    .string()
+    .min(1, 'Name must have at least 1 characters.')
+    .transform((lastName) => lastName.replace(/[0-9]/g, '')),
+  email: z.string().email('Write a valid email.'),
   password: z
     .string()
-    .min(1, { message: 'Description must have at least 1 character.' })
-    .max(50, { message: 'Description must have at most 50 characters.' }),
+    .min(8, 'Password must have at least 8 characters.')
+    .nullable(),
 })
 
 type ProfileFormValues = z.infer<typeof profileSchema>
 
 const ProfileForm = () => {
+  const [inputType, setInputType] = useState('password')
   const hashedCPF = Cookies.get('hashedCPF') ?? ''
   const user = useUser(hashedCPF)
 
@@ -33,39 +40,65 @@ const ProfileForm = () => {
   } = useForm<ProfileFormValues>({
     resolver: zodResolver(profileSchema),
     defaultValues: {
-      name: user.data?.firstName ?? 'Name',
-      email: '',
-      password: '',
+      firstName: user.data?.firstName ?? 'First name',
+      lastName: user.data?.lastName ?? 'Last name',
+      email: user.data?.email ?? 'Email',
     },
   })
 
+  const updateUser = useUpdateUser()
+
   const onSubmit = (data: ProfileFormValues) => {
+    updateUser.mutate({
+      hashedCPF,
+      ...data,
+    })
     reset()
   }
 
   return (
     <>
-      <div className="flex h-96 items-center gap-56 border-b border-zinc-800 px-40 text-white ">
+      <div className="flex h-[600px] items-center gap-56 border-b border-zinc-800 px-40 text-white ">
         <span className="ml-1 text-2xl">Profile</span>
         <div className=" flex w-[300px] flex-col">
           <form onSubmit={handleSubmit((data) => onSubmit(data))}>
             <div>
               <label
-                htmlFor="name"
+                htmlFor="firstName"
                 className="text-md font-semibold text-white text-opacity-70"
               >
-                Name
+                First name
               </label>
               <input
                 type="text"
-                {...register('name')}
-                placeholder={user.data?.firstName ?? 'Name'}
+                {...register('firstName')}
+                placeholder=""
                 className="mb-2 mt-3 w-full border-b border-white border-opacity-20 bg-zinc-950 py-2 text-lg font-semibold outline-none"
                 onInput={handleNameInput}
               />
-              {errors.name && (
+              {errors.firstName && (
                 <span className="text-sm text-red-500">
-                  {errors.name.message}
+                  {errors.firstName.message}
+                </span>
+              )}
+            </div>
+            <div>
+              <label
+                htmlFor="lastName"
+                className="text-md font-semibold text-white text-opacity-70"
+              >
+                Last name
+              </label>
+              <input
+                type="text"
+                {...register('lastName')}
+                placeholder=""
+                className="mb-2 mt-3 w-full border-b border-white border-opacity-20 bg-zinc-950 py-2 text-lg font-semibold outline-none"
+                onInput={handleNameInput}
+              />
+              {errors.lastName && (
+                <span className="text-sm text-red-500">
+                  {errors.lastName.message}
                 </span>
               )}
             </div>
@@ -79,12 +112,35 @@ const ProfileForm = () => {
               <input
                 type="text"
                 {...register('email')}
-                placeholder="Eg. Withdraw for bill payment."
+                placeholder=""
                 className="mb-2 mt-3 w-full border-b border-white border-opacity-20 bg-zinc-950 py-2 text-lg font-semibold outline-none"
               />
               {errors.email && (
                 <span className="text-sm text-red-500">
                   {errors.email.message}
+                </span>
+              )}
+            </div>
+            <div className="mb-3">
+              <label
+                htmlFor="password"
+                className="text-md flex items-center gap-4 font-semibold text-white text-opacity-70"
+              >
+                <span>Password</span>
+                <PasswordToggle
+                  inputType={inputType}
+                  setInputType={setInputType}
+                />
+              </label>
+              <input
+                type={inputType}
+                {...register('password')}
+                placeholder="********"
+                className="mb-0.5 mt-3 w-full border-b border-white border-opacity-20 bg-zinc-950 py-2 text-lg outline-none"
+              />
+              {errors.password && (
+                <span className="text-sm text-red-500">
+                  {errors.password.message}
                 </span>
               )}
             </div>
