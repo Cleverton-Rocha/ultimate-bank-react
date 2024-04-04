1e306f8a37b63bbffda4458003f1e7a2beae0729
ultr4 <cl.rch09@icloud.com>
2024-04-04 00:48:48 -0300

    feat: Adiciona função useUpdatePassword para atualizar senha do usuário

--- src/queries/user/index.ts
+++ src/queries/user/index.ts
@@ -3,10 +3,16 @@ import Cookies from 'js-cookie'
 import { AxiosError } from 'axios'
 import toast from 'react-hot-toast'
 
-import { login, register, getUser, updateUser } from '../../services/api'
+import {
+  login,
+  register,
+  getUser,
+  updateUser,
+  updatePassword,
+} from '../../services/api'
 import { LoginFormValues } from '../../components/login-form'
 import { RegisterFormValues } from '../../components/register-form'
-import { UpdateUserData } from '../../utils/types'
+import { UpdatePasswordData, UpdateUserData } from '../../utils/types'
 import queryClient from '../../services/queryClient'
 
 export function useRegister() {
@@ -62,6 +68,26 @@ export function useUpdateUser() {
     onSuccess: () => {
       toast.success('Profile updated successfully!')
       queryClient.invalidateQueries({ queryKey: ['user'] })
+    },
+    onError: (error: AxiosError) => {
+      const errorMessage = error.response?.data as string
+      toast.error(errorMessage)
+    },
+  })
+}
+
+export function useUpdatePassword() {
+  return useMutation({
+    mutationFn: (data: UpdatePasswordData) => {
+      return updatePassword(data)
+    },
+    onSuccess: () => {
+      toast.success('You must login again.')
+      toast.success('Password updated successfully!')
+      queryClient.invalidateQueries({ queryKey: ['user'] })
+      setTimeout(() => {
+        window.location.href = '/'
+      }, 1500)
     },
     onError: (error: AxiosError) => {
       const errorMessage = error.response?.data as string
